d32667051237bd9384a1e49b8397afe8424d98f4
Federico Martinez Account <mar15067@byui.edu>
2019-06-08 20:37:04 -0700

    udate to lesson 6

--- lesson-6/js/currentdate.js
+++ lesson-6/js/currentdate.js
@@ -1,4 +1,10 @@
-function toogleMenue() {
-
-    document.getElementById("primaryNav").classList.toggle(hide);
+function displayDate() {
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    var dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+			
+	var newDate = new Date();
+			
+	var formattedDate = dayNames[newDate.getDay()] + ',' +' ' + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear();
+			
+	document.getElementById("currentdate").innerHTML = formattedDate;
 }
