5cfd68c2d569539b8847ee4d1a3834d0f7e9a126
martinezcazenave <mar15067@byui.edu>
2020-05-13 19:56:19 -0700

    wednesday update

---
+++ lesson4/js/last.update.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+var x =document.lastModified;
+document.getElementById("lastupdated").innerHTML = "Last Updated:" + x;
