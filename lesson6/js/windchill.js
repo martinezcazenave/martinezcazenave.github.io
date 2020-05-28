4a495ddebb3be4640503adee8eb326ea2e018ea9
martinezcazenave <mar15067@byui.edu>
2020-05-27 18:28:48 -0700

    Wednesday update 2

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -4,7 +4,7 @@ var s= parseFloat(document.getElementById("value2").textContent);
 function WindChill(){
 var f= Math.round(35.74 + (0.6215*t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));
 
-if (f < 50) {
+if (t < 50 && s < 3) {
     document.getElementById("value3").innerHTML= f + "&#xb0;F";  
 }
 else {
