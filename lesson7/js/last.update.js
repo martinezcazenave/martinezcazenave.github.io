3d697297e7162c19aee3666b85f666d0005f97db
martinezcazenave <mar15067@byui.edu>
2020-06-04 17:18:41 -0700

    Thursday update 1

---
+++ lesson7/js/last.update.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById(
+        "currentdate"
+        ).textContent = new Date().toLocaleDateString("en-gb", options);
+} catch (e) {
+    alert ("error")
+}
+
+
+
+    var date = new Date();
+    var dayOfWeek = date.getDay();
+  
+    if (dayOfWeek == 5) {
+      var pancake = document.getElementById("pancake");
+      pancake.style.display = "block";
+    }
+  
