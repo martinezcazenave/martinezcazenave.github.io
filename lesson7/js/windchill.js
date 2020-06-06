ce980e2c4f0e74abfd56f0c721a428f741474af2
martinezcazenave <mar15067@byui.edu>
2020-06-06 11:22:54 -0700

    saturday upload 1

--- lesson7/js/windchill.js
+++ lesson7/js/windchill.js
@@ -1,10 +1,9 @@
-
 var t= parseFloat(document.getElementById("value1").textContent);
 var s= parseFloat(document.getElementById("value2").textContent);
 function WindChill(){
-var f= Math.round(35.74 + (0.6215*t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));
+var f= Math.round((35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)) * 10)/10;
 
-if (t < 50 && s < 3) {
+if (t <= 50 && s > 3) {
     document.getElementById("value3").innerHTML= f + "&#xb0;F";  
 }
 else {
@@ -14,5 +13,3 @@ else {
 
 }
 WindChill();
-
-
