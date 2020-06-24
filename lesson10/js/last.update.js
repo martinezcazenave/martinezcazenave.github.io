10a5c576be33bc7ea6006ad55079d3abe2009414
martinezcazenave <mar15067@byui.edu>
2020-06-23 19:57:10 -0700

    tuesday update

---
+++ lesson10/js/last.update.js
@@ -0,0 +1,103 @@
+function toggleMenu() {
+
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById(
+        "currentdate"
+        ).textContent = new Date().toLocaleDateString("en-gb", options);
+} catch (e) {
+    alert ("error")
+}
+
+
+
+    var date = new Date();
+    var dayOfWeek = date.getDay();
+  
+    if (dayOfWeek == 5) {
+      var pancake = document.getElementById("pancake");
+      pancake.style.display = "block";
+    }
+  
+    function adjustRating(rating) {
+        document.getElementById("ratingvalue").innerHTML = rating;
+    }
+
+
+    const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const cities = jsonObject['towns'];
+    
+    for (let i = 0; i < cities.length; i++) {
+    let place = cities[i]
+    let townname = ["Fish Haven", "Preston", "Soda Springs"]
+        if (townname.includes(place.name)) {
+       // creating an article section 
+        let section = document.createElement('article');
+        section.classList.add('maincontainer')
+        // creating first div
+        let containtext = document.createElement('div');
+        containtext.classList.add('textcol');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('h3');
+        let year = document.createElement('p');
+        let population = document.createElement('p');
+        let rainfall = document.createElement('p');
+          
+        h2.textContent = place.name;
+        motto.textContent = place.motto;
+        year.textContent = 'Year Founded: ' + place.yearFounded;
+        population.textContent = 'Population: ' + place.currentPopulation;
+        rainfall.textContent = 'Annual Rain Fall: ' + place.averageRainfall;
+
+        containtext.appendChild(h2);
+        containtext.appendChild(motto);
+        containtext.appendChild(year);
+        containtext.appendChild(population);
+        containtext.appendChild(rainfall);
+
+            // creating second div
+          let picture = document.createElement('div');
+          picture.classList.add('homepic')
+          let image = document.createElement('img');
+          let alt = document.createElement('alt');
+          alt.setAttribute('alt', place.name);
+          image.setAttribute('src', "images/" + place.photo);
+          picture.appendChild(image)
+
+          
+          section.appendChild(containtext)
+          section.appendChild(picture)
+          
+
+        document.querySelector('div.townname').appendChild(section);
+      }
+    }
+  });
+
+  //weather API
+  const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById('current').textContent = jsObject.weather[0].main;
+        document.getElementById('temp').textContent = jsObject.main.temp;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('wind').textContent = jsObject.wind.speed;
+
+    }); 
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,14 +1,14 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd";
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        //console.log(jsObject);
+        console.log(jsObject);
         document.getElementById('current-temp').textContent = jsObject.main.temp;
 
     }); 
 
-    const imagesrc = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    const imagesrc = 'http://api.openweathermap.org/img/2.5/weather?id=5604473&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description; 
+    document.getElementById('imagesrc').textContent = imagesrc; 
+    document.getElementById('icon').setAttribute('src', imagesrc); 
     document.getElementById('icon').setAttribute('alt', desc);
