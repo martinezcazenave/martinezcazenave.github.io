750d62c6497f58805dfdc3cf4d81077ad1d46e08
martinezcazenave <mar15067@byui.edu>
2020-06-24 23:42:59 -0700

    Wednesday update 1

--- lesson10/js/last.update.js
+++ lesson10/js/last.update.js
@@ -9,13 +9,13 @@ try {
         day: "numeric",
         month: "long",
         year: "numeric"
+      
     };
-    document.getElementById(
-        "currentdate"
-        ).textContent = new Date().toLocaleDateString("en-gb", options);
+    document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-gb", options);
 } catch (e) {
     alert ("error")
 }
+
 
 
 
@@ -94,10 +94,32 @@ fetch(requestURL)
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
+        //console.log(jsObject);
         document.getElementById('current').textContent = jsObject.weather[0].main;
         document.getElementById('temp').textContent = jsObject.main.temp;
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('wind').textContent = jsObject.wind.speed;
-
     }); 
+
+    //5 day forecast
+    const apiforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd";
+    
+    fetch(apiforecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject);
+
+        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+        //console.log(fivedayforecast);
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        let day = 0;
+        fivedayforecast.forEach(screenforecast => {
+          let d = new Date(screenforecast.dt_txt);
+          document.getElementById(`forecast${day+1}`).textContent = screenforecast.main.temp;
+          document.getElementById(`dayoftheweek${day+1}`).textContent = weekdays[d.getDay()];
+          day++;
+        })
+
+      });
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,12 +3,14 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+const icon = document.querySelector('img');
         document.getElementById('current-temp').textContent = jsObject.main.temp;
 
-    }); 
+    
 
-    const imagesrc = 'https://api.openweathermap.org/img/2.5/weather?id=5604473&APPID=5c3caa1b5caabeae3d05c45c9ed42ddd' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description; 
-    document.getElementById('imagesrc').textContent = imagesrc; 
-    document.getElementById('icon').setAttribute('src', imagesrc); 
-    document.getElementById('icon').setAttribute('alt', desc);
+    const imagesrc = `https://api.openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+
+    icon.setAttribute('src', imagesrc);
+    icon.setAttribute('alt', desc);
+});
