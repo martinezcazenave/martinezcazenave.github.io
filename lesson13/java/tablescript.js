b94a81472c4ba9c3fd67c53ae9be7b478b85cb79
martinezcazenave <mar15067@byui.edu>
2020-07-20 22:23:48 -0700

    Monday upload

--- lesson13/java/tablescript.js
+++ lesson13/java/tablescript.js
@@ -5,5 +5,65 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);
+    //console.table(jsonObject);
+    const rentals = jsonObject['rentals'];
+
+
+    for (let i = 0 ; i < rentals.length; i++){
+            
+        let row = document.createElement('tr');
+          let type = document.createElement('td');
+         
+          let maxpeople = document.createElement('td');
+          let reservehalfday = document.createElement('td');
+          let reservefullday = document.createElement('td');
+          let walkinhalfday = document.createElement('td');
+          let walkinfullday = document.createElement('td');
+
+          type.textContent = rentals[i].type;
+          maxpeople.textContent = rentals[i].max_person;
+          reservehalfday.textContent = rentals[i].halfday_reservation;
+          reservefullday.textContent = rentals[i].fullday_reservation;
+          walkinhalfday.textContent = rentals[i].halfday_walkin;
+          walkinfullday.textContent = rentals[i].fullday_walkin;
+
+            row.appendChild(type);
+            row.appendChild(maxpeople);
+            row.appendChild(reservehalfday);
+            row.appendChild(reservefullday);
+            row.appendChild(walkinhalfday);
+            row.appendChild(walkinfullday);
+            
+          document.getElementById('rental_table').appendChild(row);
+          
+          
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
